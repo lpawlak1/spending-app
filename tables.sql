ee4677c63548aa4f4a1b281570e1bff4b223b923
jkbstepien <devjakub.stepien@gmail.com>
2022-03-21 15:38:23 +0100

    docker now runs init scripts from tables.sql

--- tables.sql
+++ tables.sql
@@ -1,10 +1,10 @@
 -- USER
 
-create table User (
+create table public.User (
     U_ID serial primary key,
     U_Name varchar not null,
     U_Email varchar not null,
     U_Role varchar not null,
     U_Password varchar not null,
     RegistrationDate timestamp default NOW()
-)
+);
