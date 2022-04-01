b6a042e0c343e0610cea8685b05bc7487887c26e
lpawlak1 <lpawlak@student.agh.edu.pl>
2022-04-01 11:20:00 +0200

    Added user authorization actor to home controller

--- spending_app_scala/app/controllers/HomeController.scala
+++ spending_app_scala/app/controllers/HomeController.scala
@@ -1,10 +1,15 @@
 package controllers
 
 
+import akka.actor.ActorRef
+import akka.pattern.ask
+import akka.util.Timeout
 import play.api.mvc._
+import services.UserAuthorizationActor.UserAuthorization
 
 import java.time.LocalDateTime
 import javax.inject._
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
 
@@ -15,22 +20,23 @@ import scala.language.postfixOps
 @Singleton
 class HomeController @Inject()(
                                 userDao: daos.UserDao,
-                                cc: ControllerComponents
-                              )(implicit ec: ExecutionContext) extends AbstractController(cc) {
+                                cc: ControllerComponents,
+                                @Named("user-authorization-actor") userAuthorizationActor: ActorRef
+  )(implicit ec: ExecutionContext) extends AbstractController(cc) {
+
+  implicit val timeout: Timeout = 5.seconds
 
   def index(user_id: Option[String]): Action[AnyContent] = Action.async {
     implicit request => {
-      /*Tutaj sprawdzam czy użytkownik podany w query parameter istnieje itp*/
-      (if (user_id.isEmpty) None else user_id.get.toIntOption.orElse(None)) match {
-        case Some(id) =>
-          userDao.findOnesUsername(id).map {
+      (userAuthorizationActor ? UserAuthorization(user_id)).mapTo[Future[Boolean]].flatten.map {
+        case true =>
+          userDao.findOnesUsername(user_id.get.toInt).map {
             case usr: Option[String] =>
-              if (usr.isEmpty) Redirect(LoginUtils.LOGIN_ERROR_LINK)
-              else Ok(views.html.index(LocalDateTime.now(), (1000.0, id), usr.get))
+              Ok(views.html.index(LocalDateTime.now(), (1000.0, user_id.get.toInt), usr.get))
             case _ => Redirect(LoginUtils.LOGIN_ERROR_LINK)
           }
-        case None => Future(Redirect(LoginUtils.LOGIN_ERROR_LINK))
-      }
+        case false => Future(Redirect(LoginUtils.LOGIN_ERROR_LINK))
+      }.flatten
     }
   }
 }
