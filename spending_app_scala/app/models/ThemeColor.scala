081d5027438ce94b5c52511d5532cb5477f6d4ee
lpawlak1 <lpawlak@student.agh.edu.pl>
2022-04-27 20:52:55 +0200

    [WIP] Done changing in html files and added default in ThemeColor model

--- spending_app_scala/app/models/ThemeColor.scala
+++ spending_app_scala/app/models/ThemeColor.scala
@@ -1,3 +1,11 @@
 package models
 
+object ThemeColor {
+  def default: ThemeColor = ThemeColor(0,"default", "orange.css")
+}
+
 case class ThemeColor(id: Int, name: String, filename: String)
+{
+}
+
+
