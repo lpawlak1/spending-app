d4861a50a258c611adb2c3f13861ccbc6861b852
MaOlszewska <marto12@wp.pl>
2022-05-05 18:50:34 +0200

    add pink color

--- spending_app_scala/app/controllers/AddExpenseController.scala
+++ spending_app_scala/app/controllers/AddExpenseController.scala
@@ -3,8 +3,8 @@ package controllers
 import akka.actor.ActorRef
 import akka.pattern.ask
 import akka.util.Timeout
-import daos.{CategoryDao, ExpenseDao, TuplesUnpack}
-import models.{Category, Expense}
+import daos.{CategoryDao, ExpenseDao, TuplesUnpack, UserConfigDao}
+import models.{Category, Expense, ThemeColor}
 import play.api.data.Form
 import play.api.data.Forms.{bigDecimal, mapping, nonEmptyText, number, text}
 import play.api.i18n
@@ -15,7 +15,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.inject._
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.language.postfixOps
 
 case class ExpenseForInsert(
@@ -30,6 +30,7 @@ case class ExpenseForInsert(
 
 @Singleton
 class AddExpenseController  @Inject()(
+                                     userConfigDao: UserConfigDao,
                                      categoryDAO: CategoryDao,
                                      expenseDAO: ExpenseDao,
                                      cc: ControllerComponents,
@@ -51,39 +52,46 @@ class AddExpenseController  @Inject()(
     )
   }
 
-  def index(user_id: Option[String]): Action[AnyContent] = Action.async{
+  def index(user_id: Option[String]): Action[AnyContent] = Action.async {
     implicit request => {
-      val zipped = categoryDAO.findTopLevelCategories.map(categories => {
-        (categories.foldRight(Future[Any]()){ (category, accumulator) => {
-          accumulator.zip(categoryDAO.findSubCategories(category.id))
-          }
-        },
-          categories: Seq[Category])
-      })
-
-      val ret = zipped.map(x => {
-        val (subCategories,topLevelCategories) = x
-        val categories: Future[List[Seq[Any]]] = TuplesUnpack.unpackFuture(ec)(subCategories)
-
-        categories.map(categories => {
-          categories.map(category => {
-            val subCategories = category.map(x => x.asInstanceOf[Category])
-
-            if (subCategories.nonEmpty){
-              val topLevelCategory = topLevelCategories.find(x => x.id == subCategories.headOption.get.parent_category_id.get)
-              (topLevelCategory, subCategories)
+      (userAuthorizationActor ? UserAuthorization(user_id)).mapTo[Future[Boolean]].flatten.map {
+        case true => {
+          val zipped = categoryDAO.findTopLevelCategories.map(categories => {
+            (categories.foldRight(Future[Any]()) { (category, accumulator) => {
+              accumulator.zip(categoryDAO.findSubCategories(category.id))
             }
-            else {
-              (None, Seq())
-            }
-          }).filter(x => x._1.isDefined).map(x => (x._1.get, x._2))
-        })
-      }).flatten
+            },
+              categories: Seq[Category])
+          })
 
-      ret.map(x => {
-        Ok(views.html.new_expense(BasicForm.form, user_id.get.toInt, x))
-      })
+          val ret = zipped.map(x => {
+            val (subCategories, topLevelCategories) = x
+            val categories: Future[List[Seq[Any]]] = TuplesUnpack.unpackFuture(ec)(subCategories)
 
+            categories.map(categories => {
+              categories.map(category => {
+                val subCategories = category.map(x => x.asInstanceOf[Category])
+
+                if (subCategories.nonEmpty) {
+                  val topLevelCategory = topLevelCategories.find(x => x.id == subCategories.headOption.get.parent_category_id.get)
+                  (topLevelCategory, subCategories)
+                }
+                else {
+                  (None, Seq())
+                }
+              }).filter(x => x._1.isDefined).map(x => (x._1.get, x._2))
+            })
+          }).flatten
+
+          val themeColorFuture = userConfigDao.getUsersColor(user_id.get.toInt)
+
+          val retThemeColor = Await.result(themeColorFuture, 3.seconds).getOrElse(ThemeColor.default)
+          val categories = Await.result(ret, 1.seconds)
+
+          Ok(views.html.new_expense(BasicForm.form, user_id.get.toInt, categories, retThemeColor))
+        }
+        case false => Redirect(LoginUtils.LOGIN_ERROR_LINK)
+      }
     }
   }
 
